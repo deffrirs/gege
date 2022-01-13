8713c12b372545105dd6b53d31019158f21fcdf8
deffrirs <94781619+deffrirs@users.noreply.github.com>
2022-01-13 13:06:19 +0700

    Update sewa.js
    P

--- shop/sewa.js
+++ shop/sewa.js
@@ -1,28 +1,42 @@
 const pc_sewa = () => {
 return `「 *PRICE LIST* 」
 							
-⬣ PRICE SEWABOT
+「 *OPEN JASA SEWA BOT* 」
+							
+⬣ *HARGA SEWA BOT*
 
-⬡ 1 MINGGU = Rp 5.000
-⬡ 2 MINGGU = Rp 8.000
-⬡ 3 MINGGU = Rp 10.000
-⬡ 1 BULAN = Rp 15.000
-⬡ PERMANEN = Rp 100.000
-⬡ PERMANEN + USER PREM = Rp 150.000
+⬡ 1 MINGGU = Rp 5.000,00
+⬡ 2 MINGGU = Rp 10.000,00
+⬡ 3 MINGGU = Rp 15.000,00
+⬡ 1 BULAN = Rp 20.000,00
+⬡ PERMANEN = Rp 25.000,00
 
+「*FITUR MENARIK*」
+⬣ _5 set menu keren_✔️
+⬣ _Menu stiker_✔️
+⬣ _sticker meme_✔️
+⬣ _All menu download_✔️
+⬣ _Menu Game Beragam_✔️
+⬣ _Maker menu work_✔️
+⬣ _Menu Image Effect_✔️
+⬣ _*Dan Banyak Lagi Fitur Nya*_
 
-⬣ PRICE JADIBOT
+⬣ *PRICE JADIBOT*
+Fitur Jadi Bot Juga Tersedia Dan BOT Dijamin Run Otomatis 24jam no delay Tergantung Klw Nomor Ga terbanned.
 
-⬡ JADI BOT PERMANEN 25K
+_Dibawah Berikut Harga Jadi BOT_
 
+⬡ JADI BOT 1 Bulan 10K
+⬡ Jadi BOT 2  Bulan 20k
+⬡ Jadi BOT PERMANEN 25K
 
 ⬣ MINAT? PM
-⬡ wa.me/62887435047326
+⬡ wa.me/6282132805286
 
 
 NOTE : 
 Bot On 24 Jam Karna Di Run Menggunakan Heroku
-Reset Data Setiap Jam 02:00 `
+Reset Data Setiap Jam 02:00`
 	}
 
 exports.pc_sewa = pc_sewa
